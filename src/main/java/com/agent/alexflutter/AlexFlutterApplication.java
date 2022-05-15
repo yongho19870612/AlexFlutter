97017276d1a99bfb20b74dacff8fba918dc610cb
kakao_ent <yongho612@gmail.com>
2022-05-15 23:30:41 +0900

    Initial commit

---
+++ src/main/java/com/agent/alexflutter/AlexFlutterApplication.java
@@ -0,0 +1,13 @@
+package com.agent.alexflutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexFlutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexFlutterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agent/alexflutter/AlexFlutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent.alexflutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexFlutterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
